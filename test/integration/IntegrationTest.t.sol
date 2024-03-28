e921a76bc4c82581603c7505bff5c4be786e9a06
Maximiliano Cruz <maxi7587@hotmail.com>
2024-03-28 12:14:28 -0300

    base Integartion test

--- test/integration/IntegrationTest.t.sol
+++ test/integration/IntegrationTest.t.sol
@@ -1,3 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {Test} from "forge-std/Test.sol";
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {Raffle} from "../../src/Raffle.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+
+contract RaffleIntegrationTests is Test {
+    Raffle raffle;
+    HelperConfig helperConfig;
+
+    function setUp() public {
+        DeployRaffle deployer = new DeployRaffle();
+        (raffle, helperConfig) = deployer.run();
+    }
+
+    function testDeployRaffleWorks() public {
+        assert(raffle.getPlayers().length == 0);
+    }
+
+    // TODO: test other DeployRaffle banches and add coverage to Interactions
+}
